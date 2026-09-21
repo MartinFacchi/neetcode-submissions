90673c625216bf944ea2c7fd591f779194efc0f5
Chupareo <152736671+MartinFacchi@users.noreply.github.com>
2026-09-21 09:33:23 +0100

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        map<char, int> dict_s;
+        map<char, int> dict_t;
+
+        if (s.size() != t.size()){
+            return false;
+        }
+        for (char letter : s){
+            dict_s[letter] += 1;
+        }
+        
+        for (char letter : t){
+            dict_t[letter] += 1;
+        }
+
+        for (char letter : t){
+            if (dict_t[letter] != dict_s[letter]){
+                return false;
+            }
+        }        
+
+        return true;
+    }
+};
