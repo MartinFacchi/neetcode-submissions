dcd9536e5027b31addfc0a219935d1f2f41a22b5
Chupareo <152736671+MartinFacchi@users.noreply.github.com>
2026-09-22 20:10:42 +0100

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        /*  BRUTE FORCE
+        for (int i = 0; i<nums.size();i++){
+            for (int j = 0; j<nums.size();j++){
+                if (nums[i] + nums[j] == target && i != j){
+                    return {i, j};
+                }
+            }
+        }*/
+
+        unordered_map<int,int> hash;
+        
+        for (int i = 0; i < nums.size(); i++){
+            int complement = target - nums[i];
+            
+            unordered_map<int,int>::iterator index = hash.find(complement);
+            if (index != hash.end()){
+                return {index->second, i};
+            }
+            hash[nums[i]] = i;
+        }
+
+        return {};
+    }
+};
